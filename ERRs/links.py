f400e92629e56066c60076439279b6f7c642c666
Dmitry Kachaev <dmitry.kachaev@gmail.com>
2013-04-09 01:57:18 -0400

    updated ERRs scraper

--- ERRs/links.py
+++ ERRs/links.py
@@ -1,5 +1,7 @@
-import feedparser
+#import feedparser
 import urllib2
+import chardet
+from lxml import html
 
 from lxml.html import parse
 import lxml
@@ -7,12 +9,15 @@ import json
 
 index_url="http://data.mcc.gov/raw/index.json"
 
-index=urllib2.urlopen(index_url)
+index=urllib2.urlopen(index_url).read()
 
-print index
+#index=json.dumps(json.loads(index), indent=4)
+
+index=json.loads(index)
+
+#print index
 
 errs_url="http://www.mcc.gov/pages/activities/activity/economic-rates-of-return"
-
 
 doc=parse(errs_url).getroot()
 compacts=doc.cssselect('div#activity-content-04 li a')
@@ -27,21 +32,91 @@ for c in compacts:
 	#print c.attrib['href']
 	links.append(c.attrib['href'])
 	
-#print links
+#print "Links #1"
+print links
 
+
+err_links=[]
 for link in links:
-	#print "-----------"
-	#print link
+	print "-----------"
+	print link
+
+
+	content = urllib2.urlopen(link).read()
+	encoding = chardet.detect(content)['encoding']
+	if encoding != 'utf-8':
+		content = content.decode(encoding, 'replace').encode('utf-8')
+	doc = html.fromstring(content, base_url=link)
 	
-	doc=parse(link).getroot()
+	#doc=parse(link).getroot()
 	errs=doc.cssselect('ul.unstyled li.span4 a')
 	
 	for err in errs:
 	
 		href= err.attrib["href"]
 		name=err.cssselect("span")[0].text_content()
+		#name=name.replace(u'\u2019',"'")
 		
-		#print href, name
+		err_links.append({"href":href, "name":name})
+
+# index.json {"datasets":d;"categories":c}
+
+datasets=[]
+
+for dataset in index["datasets"]:
+	if dataset["group_id"]!=4:
+		dataset["metadatalocation"]=dataset["metadatalocation"].strip()
+		datasets.append(dataset)
+
+index["datasets"]=datasets
+
+print "Errs"
+offs=0
+for link in err_links:
+	print link["href"].encode('utf-8')
+	print link["name"].encode('utf-8')
+	print "---"
+	
+	index["datasets"].append({
+            "description": link["name"].encode('utf-8'), 
+            "name": link["name"].encode('utf-8'), 
+            "location": link["href"].encode('utf-8'), 
+            "formats": ".xls", 
+            "group_id": 4, 
+            "type": "documents", 
+            "id": 9999+offs, 
+        }) 
+
+	offs=offs+1
+	
 
 
+"""
+        {
+            "description": "Senegal's Irrigation and Water Resources Management Project Delta Irrigation Activit", 
+            "name": "Senegal's Irrigation and Water Resources Management Project Delta Irrigation Activit", 
+            "location": "http://www.mcc.gov/documents/err/mcc-err-senegal-delta-irrigation.xls", 
+            "formats": ".xls", 
+            "group_id": 4, 
+            "type": "documents", 
+            "id": 1221, 
+        }, 
+
+        {
+            "metadatalocation": "http://www.mcc.gov/documents/err/mcc-err-senegal-delta-irrigation.html                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          ", 
+            "description": "Senegal's Irrigation and Water Resources Management Project Delta Irrigation Activit", 
+            "name": "Senegal's Irrigation and Water Resources Management Project Delta Irrigation Activit", 
+            "location": "http://www.mcc.gov/documents/err/mcc-err-senegal-delta-irrigation.xls", 
+            "formats": ".xls", 
+            "group_id": 4, 
+            "type": "documents", 
+            "id": 1221, 
+            "group_location": "/null"
+        }, 
+
+"""
+
+f=open("index.json","w")
+f.write(json.dumps(index,indent=4))
+f.close()
 	
